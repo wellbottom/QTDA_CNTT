88dad2ff880f6ff942e73c9ec091a6f4e4b709a1
Linh <harrykuza73@gmail.com>
2025-12-01 11:47:00 +0700

    typo FeaturedDestination

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Hero from "../components/Hero";
-import FeaturedDestination from "../components/featuredDestination";
+import FeaturedDestination from "../components/FeaturedDestination";
 import ExclusiveOffers from "../components/ExclusiveOffers";
 import Testimonial from "../components/Testimonial";
 import Footer from "../components/Footer";
